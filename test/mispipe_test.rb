2bf027f38b97514ef9671c96c3cd689a5abf631e
Corin Schedler <corin@dubdromic.com>
2016-07-07 15:38:48 -0500

    Use less verbose command in test suite

--- test/mispipe_test.rb
+++ test/mispipe_test.rb
@@ -2,7 +2,7 @@ module Moreutils
   class MispipeTest < Minitest::Test
     def test_first_command_retval
       first_command = Command.new('(exit 2)') # It's turtle(shells) all the way down
-      second_command = Command.new('ls')
+      second_command = Command.new('true')
       mispipe = Moreutils::Mispipe.new(first_command, second_command)
       assert(mispipe.run == 2)
     end
